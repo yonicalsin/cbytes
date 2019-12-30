1813539282f2ff25bda80a7107ae15d5b37c641c
yonicb <yonicb20@gmail.com>
2019-12-30 16:47:37 -0500

    ci: 🎡 Update main file, exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 /*!
- * Cbytes v1.0.0 (https://github.com/yonicb/cbytes)
+ * Cbytes v1.0.2 (https://github.com/yonicb/cbytes)
  * Copyright 2019 The Cbytes Authors
  * Copyright 2019 Yoni Calsin <@yonicb>.
  * Licensed under MIT (https://github.com/yonicb/cbytes/blob/master/LICENSE)
  */
-const hello = (name: string) => {
-    console.log(`Hello ${name}`);
-};
+import { sizeToObject } from '../lib/size-to-object';
+import { formatToObject } from '../lib/format-to-object';
+import { objectToFormat } from '../lib/object-to-format';
+import { toNumber } from '../lib/to-number';
+import { toString } from '../lib/to-string';
+import { cbytes } from '../lib';
 
-hello('Yoni Calsin');
+module.exports = cbytes;
+module.exports.cbytes = cbytes;
+module.exports.toString = toString;
+module.exports.toNumber = toNumber;
+module.exports.objectToFormat = objectToFormat;
+module.exports.formatToObject = formatToObject;
+module.exports.sizeToObject = sizeToObject;
