bd1b7b43ab2f10ff7ad2ea04f07b7ed3f27f4022
yonicb <yonicb20@gmail.com>
2019-12-30 16:51:19 -0500

    perf: ⚡️ Created build main files

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,18 +1,19 @@
 /*!
- * Cbytes v1.0.0 (https://github.com/yonicb/cbytes)
+ * Cbytes v1.0.2 (https://github.com/yonicb/cbytes)
  * Copyright 2019 The Cbytes Authors
  * Copyright 2019 Yoni Calsin <@yonicb>.
  * Licensed under MIT (https://github.com/yonicb/cbytes/blob/master/LICENSE)
  */
 "use strict";
 exports.__esModule = true;
-var size_to_object_1 = require("./lib/size-to-object");
-var format_to_object_1 = require("./lib/format-to-object");
-var object_to_format_1 = require("./lib/object-to-format");
-var to_number_1 = require("./lib/to-number");
-var to_string_1 = require("./lib/to-string");
-var lib_1 = require("./lib/index");
+var size_to_object_1 = require("../lib/size-to-object");
+var format_to_object_1 = require("../lib/format-to-object");
+var object_to_format_1 = require("../lib/object-to-format");
+var to_number_1 = require("../lib/to-number");
+var to_string_1 = require("../lib/to-string");
+var lib_1 = require("../lib");
 module.exports = lib_1.cbytes;
+module.exports.cbytes = lib_1.cbytes;
 module.exports.toString = to_string_1.toString;
 module.exports.toNumber = to_number_1.toNumber;
 module.exports.objectToFormat = object_to_format_1.objectToFormat;
